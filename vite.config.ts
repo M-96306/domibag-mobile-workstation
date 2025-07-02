fc371994f8b69f7466c8b344867d0c0c0d240ef5
M-96306 <magdabrando96@gmail.com>
2025-07-02 18:47:52 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/domibag-mobile-workstation/", // aqui o base, importante
+  base: "/",
   server: {
     host: "::",
     port: 8080,
